922760f69f7f0a455619515b7d08caba2dcbf358
Himanshu Kumar <himanshu.gfx@gmail.com>
2026-01-08 16:33:51 +0530

    feat: Add Google Analytics tracking (G-E2J25CTNXK)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -120,6 +120,19 @@ export default function RootLayout({
               <AdminAutoLogout />
               <PageViewTracker />
               <Script src="/assets/js/phosphor-icons.js" strategy="beforeInteractive" />
+              {/* Google Analytics */}
+              <Script
+                src="https://www.googletagmanager.com/gtag/js?id=G-E2J25CTNXK"
+                strategy="afterInteractive"
+              />
+              <Script id="google-analytics" strategy="afterInteractive">
+                {`
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', 'G-E2J25CTNXK');
+                `}
+              </Script>
               <SpeedInsights />
               <Analytics />
             </WishlistProvider>
